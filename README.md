6833a8350659f4e9604237462107057f5024bbe4
Nguyen Anh Quan <naquan2112@gmail.com>
2018-02-19 15:03:38 +0700

    Found the way around, without using variable fonts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# work in progress, please ignore.
+> **Project status**: *initialized* - It is not ready to be seen.
 
 # var-font-takahashi-js
 Simple Takahashi slide inplementation using variable fonts
---
+++ vftakahashi.js
@@ -0,0 +1,15 @@
+/**
+                  l e n g t h        2d  array         slide
+    c s s 2 d     using   div        s l i d e         notes
+    transform
+                  javascript         center
+                  f a d i n g        d i v
+                  animation
+
+  **/
+/*
+var test = document.getElementById("Test");
+test.style.fontSize = fontSize;
+var height = (test.clientHeight + 1) + "px";
+var width = (test.clientWidth + 1) + "px";
+*/
