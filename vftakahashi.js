5ccdca6f187ff1160b23461f448f0e45f5a33a03
Nguyen Anh Quan <naquan2112@gmail.com>
2018-02-20 17:23:32 +0700

    Layout look OK

--- vftakahashi.js
+++ vftakahashi.js
@@ -16,18 +16,52 @@ test.style.fontSize = fontSize;
 var height = (test.clientHeight + 1) + "px";
 var width = (test.clientWidth + 1) + "px";
 */
+function ratio(tagName) {
+    switch (tagName.toLowerCase()) {
+    case 'w0':
+        return 2.0*Math.sqrt(2.0);
+    case 'w1':
+        return 2.0;
+    case 'w2':
+        return Math.sqrt(2.0);
+    case 'w3':
+        return 1.0;
+    }
+    return 0.0;
+}
+
 function calibrate() {
     // get screen width and height
     var body = document.getElementsByTagName('body')[0];
     // get the slides width and height (set with css)
     var slides = document.getElementsByTagName('slide');
-    // TODO center the slides
-    slides.forEach(function(elem) {
-    });
-    // TODO foreach slide, calculate the text height
-    // TODO scale the texts
+    // center the slides
+    for (var i=0; i<slides.length; i++) {
+        slides[i].style.left = ((body.clientWidth - slides[i].clientWidth)   / 2)+'px';
+        slides[i].style.top  = ((body.clientHeight - slides[i].clientHeight) / 2)+'px';
+        var words = slides[i].children;
+        // calculate the scale from smallest wording(w3) to total
+        var scale = 0.0;
+        for (var j=0; j<words.length; j++)
+            scale += ratio(words[j].tagName);
+        // now scale and put the wording in place
+        var unit = slides[i].clientHeight / scale;
+        var curPos = 0.0;
+        for (var j=0; j<words.length; j++) {
+            words[j].style.top = curPos;
+            words[j].style.transform = "scale("+
+                (slides[i].clientWidth*1.0/words[j].clientWidth).toFixed(5)+
+                ","+
+                (unit*ratio(words[j].tagName)/words[j].clientHeight).toFixed(5)+
+                ")";
+            curPos += unit*ratio(words[j].tagName);
+        }
+    }
 }
 
+document.addEventListener("DOMContentLoaded", function(ev) {
+    calibrate();
+});
 window.addEventListener('resize', (ev) => {
     calibrate();
 });
@@ -36,3 +70,4 @@ window.addEventListener('orientationChange', (ev) => {
 });
 
 // TODO: hide/show/animate slides
+// css opacity
